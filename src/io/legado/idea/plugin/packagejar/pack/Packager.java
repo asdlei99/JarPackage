4d422b314acb56ee8fc6e0de595c7c50c0526197
gedoor <gekunfei@live.com>
2021-07-04 00:45:03 +0800

    优化

--- src/io/legado/idea/plugin/packagejar/pack/Packager.java
+++ src/io/legado/idea/plugin/packagejar/pack/Packager.java
@@ -8,5 +8,7 @@ package io.legado.idea.plugin.packagejar.pack;
 import com.intellij.openapi.compiler.CompileStatusNotification;
 
 public abstract class Packager implements CompileStatusNotification {
+
     public abstract void pack();
+
 }
